3ba5bd75f377caaedeec22e2a2cebaa3e3c85178
Abhiraj <abhirajsinghchouhan4@gmail.com>
2025-09-09 19:32:23 +0530

    backend project structure

---
+++ backend/realplates/src/main/java/io/github/abhiraj_21/realplates/RealplatesApplication.java
@@ -0,0 +1,13 @@
+package io.github.abhiraj_21.realplates;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealplatesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealplatesApplication.class, args);
+	}
+
+}
---
+++ backend/realplates/src/test/java/io/github/abhiraj_21/realplates/RealplatesApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.abhiraj_21.realplates;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealplatesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
